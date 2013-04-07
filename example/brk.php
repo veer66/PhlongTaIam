b33705a93d214f95ee0e24a8d2292511c49b77fa
Vee Satayamas <vsatayamas@gmail.com>
2013-04-07 10:38:34 +0700

    example

---
+++ example/brk.php
@@ -0,0 +1,8 @@
+<?php
+require_once "../src/WordBreaker.php";
+
+use PhlongTaIam\WordBreaker as WordBreaker;
+$wordBreaker = new WordBreaker("../data/tdict-std.txt");
+print_r($wordBreaker->breakIntoWords("ฉันกินข้าว"));
+
+?>
