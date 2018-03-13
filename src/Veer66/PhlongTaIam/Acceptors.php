9c1b6e5454823aeaa296fca8a501974446b4b81c
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-03-14 02:32:35 +0700

    FIX: PSR2 compliance

--- src/Veer66/PhlongTaIam/Acceptors.php
+++ src/Veer66/PhlongTaIam/Acceptors.php
@@ -3,53 +3,56 @@ namespace Veer66\PhlongTaIam;
 
 class Acceptors
 {
-	public $creators;
+    public $creators;
 
-	function __construct() 
-	{
-		$this->creators = array();
-		$this->current = array();
-		$this->tag = array();
-	}
+    function __construct()
+    {
+        $this->creators = array();
+        $this->current = array();
+        $this->tag = array();
+    }
 
-	function reset() {
-		$this->current = array();
-	}
+    function reset()
+    {
+        $this->current = array();
+    }
 
-	function transit($ch) {
-		foreach ($this->creators as $creator) {
-			$acceptor = $creator->createAcceptor($this->tag);
-			if (!is_null($acceptor))
-				$this->current[] = $acceptor;
-		}
+    function transit($ch)
+    {
+        foreach ($this->creators as $creator) {
+            $acceptor = $creator->createAcceptor($this->tag);
+            if (!is_null($acceptor)) {
+                $this->current[] = $acceptor;
+            }
+        }
 
 
-		$_current = array();
-		$this->tag = array();
+        $_current = array();
+        $this->tag = array();
 
-		for ($i = 0; $i < sizeof($this->current); $i++) {
-			$_acceptor = $this->current[$i];
-			$acceptor = $_acceptor->transit($ch);
+        for ($i = 0; $i < sizeof($this->current); $i++) {
+            $_acceptor = $this->current[$i];
+            $acceptor = $_acceptor->transit($ch);
 
-			if (!$acceptor->isError) {
-				$_current[] = $acceptor;
-				if (!array_key_exists($acceptor->tag, $this->tag))
-					$this->tag[$acceptor->tag] = array();
-				$this->tag[$acceptor->tag] = $acceptor;
-			}
-		}
-		$this->current = $_current;
-	}
+            if (!$acceptor->isError) {
+                $_current[] = $acceptor;
+                if (!array_key_exists($acceptor->tag, $this->tag)) {
+                    $this->tag[$acceptor->tag] = array();
+                }
+                $this->tag[$acceptor->tag] = $acceptor;
+            }
+        }
+        $this->current = $_current;
+    }
 
-	function getFinalAcceptors() 
-	{
-		$finalAcceptors = array();
-		foreach($this->current as $acceptor) {	
-			if ($acceptor->isFinal) {
-				$finalAcceptors[] = $acceptor;
-			}
-		}
-		return $finalAcceptors;
-	}
+    function getFinalAcceptors()
+    {
+        $finalAcceptors = array();
+        foreach ($this->current as $acceptor) {
+            if ($acceptor->isFinal) {
+                $finalAcceptors[] = $acceptor;
+            }
+        }
+        return $finalAcceptors;
+    }
 }
-?>
